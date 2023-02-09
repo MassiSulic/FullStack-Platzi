1da1aa1ecd26894d95b13244ac31960af6baec17
Bloki2319 <Sulic.augusto@gmail.com>
2023-02-08 21:23:44 -0300

    Subo ejercicios :)

--- Curso Basico JavaScript/Playdrounds.js
+++ Curso Basico JavaScript/Playdrounds.js
@@ -14,3 +14,26 @@ function solution(valor) {
   export function solution(secreto) {
     return (secreto > 5);
   }
+
+  // Ejercicos 3
+
+export function solution(article) {
+
+  switch (article) {
+
+    case "computadora":
+      console.log("Con mi computadora puedo programar usando JavaScript");
+      break;
+
+    case "celular":
+      console.log("En mi celular puedo aprender usando la app de Platzi");
+      break;
+
+    case "cable":
+      console.log("¡Hay un cable en mi bota!");
+      break;
+    
+    default:
+      console.log("Artículo no encontrado")
+  }
+}

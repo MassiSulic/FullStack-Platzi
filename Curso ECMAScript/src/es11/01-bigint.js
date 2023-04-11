54105c86a0511f24ffd88829f1d25776d78c1b55
MassiSulic <massimo.a.sulic@gmail.com>
2023-04-11 16:33:30 -0300

    es11

---
+++ Curso ECMAScript/src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumbre = 1283746891237461235n;
+const anotherBigNumber = BigInt(1283746891237461235);
+console.log(aBigNumbre);
+console.log(anotherBigNumber);
---
+++ Curso ECMAScript/src/es11/04-globalthis.js
@@ -0,0 +1,4 @@
+console.log(window); // Navegador
+console.log(global); // node
+console.log(selft); // webworker
+console.log(globalThis); // Nueva opcion que encierra todas las posibilidades
